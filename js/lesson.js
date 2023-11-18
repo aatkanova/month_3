f90865f665f551b894222a89b64282044eba6373
atkanova <atkanovaaijana@gmail.com>
2023-11-18 12:57:35 +0600

    third homework

--- js/lesson.js
+++ js/lesson.js
@@ -17,3 +17,58 @@ phoneButton.addEventListener('click', () => {
         phoneSpan.style.color = 'red';  
     }
 })
+
+
+//TAB SLIDER  
+
+const tabsContentCards = document.querySelectorAll('.tab_content_block');
+const tabsItems = document.querySelectorAll('.tab_content_item');
+const tabsItemsParent = document.querySelector('.tab_content_items');
+let intervalId;
+
+const hideTabsContentCards = () => {
+    tabsContentCards.forEach((tabContentCard) => {
+        tabContentCard.style.display = 'none';
+    });
+    tabsItems.forEach((tabItem) => {
+        tabItem.classList.remove('tab_content_item_active');
+    });
+};
+
+const showTabsContentCards = (indexElement = 0) => {
+    tabsContentCards[indexElement].style.display = 'block';
+    tabsItems[indexElement].classList.add('tab_content_item_active');
+};
+
+hideTabsContentCards();
+showTabsContentCards();
+
+tabsItemsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabsItems.forEach((tabItem, tabItemIndex) => {
+            if (event.target === tabItem) {
+                clearInterval(intervalId); 
+                hideTabsContentCards();
+                showTabsContentCards(tabItemIndex);
+                intervalId = setInterval(() => { 
+                    tabItemIndex++; 
+                    if (tabItemIndex > tabsItems.length - 1) {
+                        tabItemIndex = 0;
+                    }
+                    hideTabsContentCards();
+                    showTabsContentCards(tabItemIndex);
+                }, 3000);
+            }
+        });
+    }
+};
+
+let index = 0;
+intervalId = setInterval(() => { // Начинаем интервал с нулевой вкладки
+    index++;
+    if (index > tabsItems.length - 1) {
+        index = 0;
+    }
+    hideTabsContentCards();
+    showTabsContentCards(index);
+}, 3000);
